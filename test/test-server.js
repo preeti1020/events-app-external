ad36ff9768fff9f8efe4be99028d3fec11ab048b
preeti1020 <72933268+preeti1020@users.noreply.github.com>
2020-10-15 12:12:57 -0400

    Update test-server.js

--- test/test-server.js
+++ test/test-server.js
@@ -14,8 +14,8 @@ describe('GET /', function () {
       .reply(200, {
         "status": 200,
         "events": [
-          { "title": 'an event', "id": 1234, "description": 'something really cool CHANGE' },
-          { "title": 'another event', id: 5678, "description": 'something even cooler CHANGE' }
+          { "title": 'an event', "id": 1234, "description": 'something really cool NEW BRANCH TEST' },
+          { "title": 'another event', id: 5678, "description": 'something even cooler NEW BRANCH TEST' }
         ]
       });
 
